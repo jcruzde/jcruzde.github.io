6ede8044b880f8940dde5d420061a6d56bc0c837
Jorge Cruz <43792314+jcruzde@users.noreply.github.com>
2020-12-14 00:37:53 +0100

    Update ajax2.js

--- ajax2.js
+++ ajax2.js
@@ -7,7 +7,9 @@ xhttp.onreadystatechange = function (){
     if(this.readyState == 4 && this.status == 200){
         console.log(this.responseText);
         let datos = JSON.parse(this.responseText);
+        console.log('Ahora lo imnpro en modo JSON');
         console.log(datos);
+        console.log('IMPRESO!');
     }
 }
 

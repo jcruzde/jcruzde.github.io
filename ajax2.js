7dd55dd850667f5af293375b9a025813c647484b
Jorge Cruz <43792314+jcruzde@users.noreply.github.com>
2020-12-14 01:04:15 +0100

    Update ajax2.js

--- ajax2.js
+++ ajax2.js
@@ -1,15 +1,16 @@
-console.log("correcto");
 
 const xhttp = new XMLHttpRequest();
 xhttp.open('GET', 'catalogo.json', true);
 xhttp.send();
 xhttp.onreadystatechange = function (){
     if(this.readyState == 4 && this.status == 200){
-        console.log(this.responseText);
         let datos = JSON.parse(this.responseText);
-        console.log('Ahora lo imnpro en modo JSON');
         console.log(datos);
-        console.log('IMPRESO!');
+
+        for(let item of datos){
+            console.log(item);
+        }
+    
     }
 }
 

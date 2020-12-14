ca196bf4140785774c70d61fb3a942ee650e28a5
Jorge Cruz <43792314+jcruzde@users.noreply.github.com>
2020-12-14 01:11:29 +0100

    update

--- ajax2.js
+++ ajax2.js
@@ -5,10 +5,15 @@ xhttp.send();
 xhttp.onreadystatechange = function (){
     if(this.readyState == 4 && this.status == 200){
         let datos = JSON.parse(this.responseText);
-        console.log(datos);
 
+        let res = document.querySelector('#education_block');
+        res.innerHTML = '';
         for(let item of datos){
-            console.log(item);
+            res.innerHTML += `
+                <h4>${item.titulo}</h4>
+                <h5>${item.artista}</h5>
+            
+            `
         }
     
     }
